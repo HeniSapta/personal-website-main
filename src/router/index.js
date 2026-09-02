b4ccd8ac7bd4e3c75796b384f6c02640e5445623
hnyspta <henisaptamawar@students.amikom.ac.id>
2026-09-03 01:33:51 +0700

    Configure router for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ResumeView from "@/views/ResumeView.vue";
 import ProjectView from "@/views/ProjectView.vue";
 import ContactView from "@/views/ContactView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
