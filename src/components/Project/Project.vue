c61f3f957fb4743b78c70137888999dcdc465fdb
hnyspta <henisaptamawar@students.amikom.ac.id>
2026-09-05 03:17:48 +0700

    Update project links

--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -15,10 +15,10 @@ import projectSpcm from '@/assets/images/project/spcm.png'
               <h2 class="fw-bolder">Machine Learning</h2>
               <p>Analisis sentimen opini publik terhadap UU TPKS menggunakan TF-IDF dan machine learning dengan akurasi terbaik 85,35% pada Random Forest dan SVM.</p>
               <div style="margin-bottom: 15px;">
-                <a href="https://bit.ly/GoogleColabJurnal" class="text-gradient text-decoration-none" target="_blank">https://colab.research.google.com/drive/17YpDZZtSXlmDji0Lmf64GYldUCt8AyQz?usp=sharing</a>
+                <a href="https://bit.ly/GoogleColabJurnal" class="text-gradient text-decoration-none" target="_blank">https://bit.ly/GoogleColabJurnal</a>
               </div>
               <div>
-                <a href="https://bit.ly/DOIJurnalJAIC" class="text-gradient text-decoration-none" target="_blank">https://doi.org/10.30871/jaic.v9i6.11503</a>
+                <a href="https://bit.ly/DOIJurnalJAIC" class="text-gradient text-decoration-none" target="_blank">https://bit.ly/DOIJurnalJAIC</a>
               </div>
             
               </div>
@@ -34,7 +34,7 @@ import projectSpcm from '@/assets/images/project/spcm.png'
             <div class="col-md-6 p-5">
               <h2 class="fw-bolder">Data Analytics Capstone Project</h2>
               <p>Menganalisis data penjualan Wonka Chocolate Factory untuk mengidentifikasi tren penjualan, performa produk, dan peluang peningkatan penjualan melalui data cleaning, visualization, dan data analysis.</p>
-              <a href="https://bit.ly/CapstoneProjectDAReVou" class="text-gradient text-decoration-none" target="_blank">https://drive.google.com/file/d/19OEjui5z9JWcoGPNRoUlZaBGtC6p9tig/view?usp=sharing</a>
+              <a href="https://bit.ly/CapstoneProjectDAReVou" class="text-gradient text-decoration-none" target="_blank">https://bit.ly/CapstoneProjectDAReVou</a>
             </div>
             <div class="col-md-6" :style="{ backgroundImage: `url(${projectDb})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center' }">
             </div>
@@ -49,7 +49,7 @@ import projectSpcm from '@/assets/images/project/spcm.png'
             <div class="col-md-6 p-5">
               <h2 class="fw-bolder">Analisis Faktor Risiko Penyakit Jantung</h2>
               <p>Menganalisis faktor risiko penyakit jantung menggunakan Python melalui EDA, visualisasi data, analisis korelasi Spearman, dan regresi linear untuk menemukan pola serta hubungan antar variabel.</p>
-              <a href="https://bit.ly/GoogleColabAFRPJ" class="text-gradient text-decoration-none" target="_blank">https://colab.research.google.com/drive/1-toRpViVpFZdm8yqgrmMaMQGHjLkJ5d4?usp=sharing</a>
+              <a href="https://bit.ly/GoogleColabAFRPJ" class="text-gradient text-decoration-none" target="_blank">https://bit.ly/GoogleColabAFRPJ</a>
             </div>
             <div class="col-md-6" :style="{ backgroundImage: `url(${projectSpcm})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center' }">
             </div>
