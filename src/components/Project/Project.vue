035f9fcffcc3d852b306a8ef254b736c9fc9cf91
hnyspta <henisaptamawar@students.amikom.ac.id>
2026-09-05 03:02:36 +0700

    Update portfolio

--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -13,10 +13,10 @@
               <h2 class="fw-bolder">Machine Learning</h2>
               <p>Analisis sentimen opini publik terhadap UU TPKS menggunakan TF-IDF dan machine learning dengan akurasi terbaik 85,35% pada Random Forest dan SVM.</p>
               <div style="margin-bottom: 15px;">
-                <a href="https://colab.research.google.com/drive/17YpDZZtSXlmDji0Lmf64GYldUCt8AyQz?usp=sharing" class="text-gradient text-decoration-none" target="_blank">https://colab.research.google.com/drive/17YpDZZtSXlmDji0Lmf64GYldUCt8AyQz?usp=sharing</a>
+                <a href="https://bit.ly/GoogleColabJurnal" class="text-gradient text-decoration-none" target="_blank">https://colab.research.google.com/drive/17YpDZZtSXlmDji0Lmf64GYldUCt8AyQz?usp=sharing</a>
               </div>
               <div>
-                <a href="https://doi.org/10.30871/jaic.v9i6.11503" class="text-gradient text-decoration-none" target="_blank">https://doi.org/10.30871/jaic.v9i6.11503</a>
+                <a href="https://bit.ly/DOIJurnalJAIC" class="text-gradient text-decoration-none" target="_blank">https://doi.org/10.30871/jaic.v9i6.11503</a>
               </div>
             
               </div>
@@ -32,7 +32,7 @@
             <div class="col-md-6 p-5">
               <h2 class="fw-bolder">Data Analytics Capstone Project</h2>
               <p>Menganalisis data penjualan Wonka Chocolate Factory untuk mengidentifikasi tren penjualan, performa produk, dan peluang peningkatan penjualan melalui data cleaning, visualization, dan data analysis.</p>
-              <a href="https://drive.google.com/file/d/19OEjui5z9JWcoGPNRoUlZaBGtC6p9tig/view?usp=sharing" class="text-gradient text-decoration-none" target="_blank">https://drive.google.com/file/d/19OEjui5z9JWcoGPNRoUlZaBGtC6p9tig/view?usp=sharing</a>
+              <a href="https://bit.ly/CapstoneProjectDAReVou" class="text-gradient text-decoration-none" target="_blank">https://drive.google.com/file/d/19OEjui5z9JWcoGPNRoUlZaBGtC6p9tig/view?usp=sharing</a>
             </div>
             <div class="col-md-6" style="background-image: url('/images/project/db.png'); background-size: contain; background-repeat: no-repeat; background-position: center center; ">
             </div>
@@ -47,7 +47,7 @@
             <div class="col-md-6 p-5">
               <h2 class="fw-bolder">Analisis Faktor Risiko Penyakit Jantung</h2>
               <p>Menganalisis faktor risiko penyakit jantung menggunakan Python melalui EDA, visualisasi data, analisis korelasi Spearman, dan regresi linear untuk menemukan pola serta hubungan antar variabel.</p>
-              <a href="hhttps://colab.research.google.com/drive/1-toRpViVpFZdm8yqgrmMaMQGHjLkJ5d4?usp=sharing" class="text-gradient text-decoration-none" target="_blank">https://colab.research.google.com/drive/1-toRpViVpFZdm8yqgrmMaMQGHjLkJ5d4?usp=sharing</a>
+              <a href="https://bit.ly/GoogleColabAFRPJ" class="text-gradient text-decoration-none" target="_blank">https://colab.research.google.com/drive/1-toRpViVpFZdm8yqgrmMaMQGHjLkJ5d4?usp=sharing</a>
             </div>
             <div class="col-md-6" style="background-image: url('/images/project/spcm.png'); background-size: contain; background-repeat: no-repeat; background-position: center center; ">
             </div>
